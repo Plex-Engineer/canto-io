17bb73f5acc154999aae8d6f150fc5fcd0df2547
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-09-06 13:58:53 -0400

    jest with vite config files

---
+++ src/tests/sample.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+  it("should work as expected", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
