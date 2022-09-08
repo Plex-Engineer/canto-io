b93f6a9deefe875c26bf7b2fd8f0f5af2abf2335
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-09-08 15:21:41 -0400

    canto docs to footer

--- src/global/components/Footer.tsx
+++ src/global/components/Footer.tsx
@@ -1,11 +1,17 @@
 import "./footer.css";
 import discordIcon from "../../assets/discordIcon.png";
 import gitHubIcon from "../../assets/githubIcon.png";
+import cantoIcon from "../../assets/Favicon.svg";
 export const Footer = () => {
   return (
     <footer id="footer" className="site-footer" role="contentinfo">
       <div className="social-wrapper">
         <ul>
+          <li>
+            <a href="https://docs.canto.io/">
+              <img src={cantoIcon} alt="Canto Logo" className="social-icon" />
+            </a>
+          </li>
           <li>
             <a href="https://twitter.com/CantoPublic">
               <img
