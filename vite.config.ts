808f742a17b804ede07f20498392b6b1fba45ed6
Bharadwaj Palakurthy <bharadwajpalakurthy@gmail.com>
2022-09-23 20:00:16 +0530

    addded yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 //vite.config.js
-import nodePolyfills from 'rollup-plugin-polyfill-node';
-const production = process.env.NODE_ENV === 'production';
+import nodePolyfills from "rollup-plugin-polyfill-node";
+const production = process.env.NODE_ENV === "production";
 export default {
-  server: { 
-    host: '0.0.0.0'
-  } ,
+  server: {
+    host: "0.0.0.0",
+    port: "8000",
+  },
   plugins: [
-    react(), tsconfigPaths(),
+    react(),
+    tsconfigPaths(),
     // ↓ Needed for development mode
-    !production && nodePolyfills({
-        include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js')]
-      })
+    !production &&
+      nodePolyfills({
+        include: [
+          "node_modules/**/*.js",
+          new RegExp("node_modules/.vite/.*js"),
+        ],
+      }),
   ],
 
   build: {
     rollupOptions: {
       plugins: [
         // ↓ Needed for build
-        nodePolyfills()
-      ]
+        nodePolyfills(),
+      ],
     },
     // ↓ Needed for build if using WalletConnect and other providers
     commonjsOptions: {
-      transformMixedEsModules: true
-    }
-  }
-}
+      transformMixedEsModules: true,
+    },
+  },
+};
