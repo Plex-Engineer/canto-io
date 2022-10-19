5b95b540527e9392c76cdab5269349a83ffbe669
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2022-10-19 17:08:52 +0530

    added navbar with animations

--- src/pages/bridge/TransactionsTab.tsx
+++ src/pages/bridge/TransactionsTab.tsx
@@ -111,7 +111,7 @@ const TransactionsTab = () => {
         pendingBridgeTransactions.length ==
         0 && (
         <NotConnected
-          title="No Trasacntions"
+          title="No Transactions"
           subtext="You haven't made any transactions using bridging yet."
           buttonText="Get Started"
           bgFilled
