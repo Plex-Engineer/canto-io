3199db10bd0d51022c1a8a7fe8116a2bab50ef7b
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-10-29 14:21:04 -0400

    keplr gas steps

--- src/global/packages/src/utils/walletFunctionality.ts
+++ src/global/packages/src/utils/walletFunctionality.ts
@@ -39,7 +39,7 @@ export async function addCantoToKeplr() {
             coinDecimals: 18,
             // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
             // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-            // coinGeckoId: ""
+            coinGeckoId: "canto",
           },
           // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
           // The 'stake' button in Keplr extension will link to the webpage.
@@ -48,7 +48,7 @@ export async function addCantoToKeplr() {
           bip44: {
             // You can only set the coin type of BIP44.
             // 'Purpose' is fixed to 44.
-            coinType: 118,
+            coinType: 60,
           },
           // Bech32 configuration to show the address to user.
           // This field is the interface of
@@ -79,7 +79,7 @@ export async function addCantoToKeplr() {
               coinDecimals: 18,
               // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
               // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
+              coinGeckoId: "canto",
             },
           ],
           // List of coin/tokens used as a fee token in this chain.
@@ -93,7 +93,12 @@ export async function addCantoToKeplr() {
               coinDecimals: 18,
               // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
               // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
+              coinGeckoId: "canto",
+              gasPriceStep: {
+                low: 125000000000,
+                average: 250000000000,
+                high: 375000000000,
+              },
             },
           ],
           // (Optional) The number of the coin type.
@@ -101,16 +106,18 @@ export async function addCantoToKeplr() {
           // Ideally, it is recommended to be the same with BIP44 path's coin type.
           // However, some early chains may choose to use the Ccantos Hub BIP44 path of '118'.
           // So, this is separated to support such chains.
-          coinType: 118,
+          coinType: 60,
           // (Optional) This is used to set the fee of the transaction.
           // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
           // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
           // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
-          gasPriceStep: {
-            low: 1250000000000,
-            average: 2500000000000,
-            high: 3750000000000,
-          },
+
+          features: [
+            "ibc-transfer",
+            "ibc-go",
+            "eth-address-gen",
+            "eth-key-sign",
+          ],
         });
       } catch {
         alert("Failed to suggest the chain");
@@ -137,7 +144,6 @@ export async function addCantoToKeplr() {
   // But, currently, Keplr extension manages only one address/public key pair.
   // XXX: This line is needed to set the sender address for SigningCcantosClient.
   const accounts = await offlineSigner.getAccounts();
-  console.log(accounts)
 
   // Initialize the gaia api with the offline signer that is injected by Keplr extension.
   // const cosmJS = new SigningCosmosClient(
