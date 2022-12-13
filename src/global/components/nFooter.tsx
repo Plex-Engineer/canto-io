1d98868b9dc6d5a91684f024e85d74fbb305263b
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-12-13 16:56:48 -0500

    canny feedback link

--- src/global/components/nFooter.tsx
+++ src/global/components/nFooter.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <Styled>
       <div className="links">
-        <a href="https://forms.gle/gkr5pDZYX8ZRWYWJ8">
+        <a href="https://canto.canny.io/">
           <Text type="text">report bug</Text>
         </a>
         <a href="https://docs.canto.io/">
