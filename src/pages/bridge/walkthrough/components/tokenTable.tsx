9606ebcf83e9326753ec3d1012b11700017b9ef7
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-01-26 17:41:59 -0600

    gravity

--- src/pages/bridge/walkthrough/components/tokenTable.tsx
+++ src/pages/bridge/walkthrough/components/tokenTable.tsx
@@ -44,7 +44,7 @@ const TokenTable = ({ tokens, onClose }: Props) => {
           ethereum
         </Text>
         <Text type="text" size="title3">
-          gBridge
+          gravity
         </Text>
         <Text type="text" size="title3">
           canto
