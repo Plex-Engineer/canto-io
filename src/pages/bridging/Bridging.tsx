3a71a9228dea847d354f846d4903262e57ba304a
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-03-09 12:32:17 -0500

    new custom bridge tx hooks

--- src/pages/bridging/Bridging.tsx
+++ src/pages/bridging/Bridging.tsx
@@ -7,11 +7,13 @@ import Transactions from "./Transactions";
 import { useTransactionHistory } from "./hooks/useTransactionHistory";
 import { useNetworkInfo } from "global/stores/networkInfo";
 import { createConvertTransactions } from "./utils/utils";
+import { useBridgingTransactions } from "./hooks/useBridgingTransactions";
 
 const Bridging = () => {
   const networkInfo = useNetworkInfo();
   const bridgingTokens = useBridgeTokenInfo();
   const bridgingTxs = useTransactionHistory();
+  const bri = useBridgingTransactions();
   return (
     <div>
       <CantoTabs
---
+++ src/pages/bridging/hooks/useBridgingTransactions.ts
@@ -0,0 +1,150 @@
+import { TransactionState, useContractFunction } from "@usedapp/core";
+import { Contract, utils } from "ethers";
+import { ERC20Abi, gravityBridgeAbi } from "global/config/abi";
+import { chain, convertFee, ibcFee, memo } from "global/config/cosmosConstants";
+import { CantoMainnet } from "global/config/networks";
+import { useState } from "react";
+import {
+  txConvertCoin,
+  txConvertERC20,
+} from "../utils/convertCoin/convertTransactions";
+import { checkCosmosTxConfirmation } from "global/utils/cantoTransactions/checkCosmosConfirmation";
+import { txIBCTransfer } from "../utils/IBC/IBCTransfer";
+import { BridgeOutNetworkInfo } from "../config/bridgeOutNetworks";
+
+interface BridgeTransaction {
+  state: TransactionState;
+  send: (...args: any[]) => Promise<unknown>;
+  resetState: () => void;
+}
+interface BridgingTransactionsSelector {
+  bridgeIn: {
+    approveToken: (tokenAddress: string) => BridgeTransaction;
+    sendToCosmos: (gravityAddress: string) => BridgeTransaction;
+  };
+  convertCoin: {
+    convertTx: (tokenName: string, convertIn: boolean) => BridgeTransaction;
+  };
+  bridgeOut: {
+    ibcOut: (tokenDenom: string) => BridgeTransaction;
+  };
+}
+
+export function useBridgingTransactions(): BridgingTransactionsSelector {
+  function useApprove(tokenAddress: string) {
+    const erc20Interface = new utils.Interface(ERC20Abi);
+    const contract = new Contract(tokenAddress, erc20Interface);
+    const { state, send, resetState } = useContractFunction(
+      contract,
+      "approve",
+      {
+        transactionName: "enable token",
+      }
+    );
+    return { state: state.status, send, resetState };
+  }
+  function useSendToCosmos(gravityAddress: string) {
+    const gBridgeInterface = new utils.Interface(gravityBridgeAbi);
+    const contract = new Contract(gravityAddress, gBridgeInterface);
+    const { state, send, resetState } = useContractFunction(
+      contract,
+      "sendToCosmos",
+      {
+        transactionName: "sending to cosmos",
+      }
+    );
+    return { state: state.status, send, resetState };
+  }
+  /**
+   * @notice If convertIn, tokenName must be its IBC denom
+   * @notice If convertOut, tokenName must be its EVM address
+   */
+  function useConvertCoin(tokenName: string, convertIn: boolean) {
+    const [convertState, setConvertState] = useState<TransactionState>("None");
+    const send = async (cantoAddress: string, amount: string) => {
+      setConvertState("PendingSignature");
+      try {
+        const tx = convertIn
+          ? await txConvertCoin(
+              cantoAddress,
+              tokenName,
+              amount,
+              CantoMainnet.cosmosAPIEndpoint,
+              convertFee,
+              chain,
+              memo
+            )
+          : await txConvertERC20(
+              tokenName,
+              amount,
+              cantoAddress,
+              CantoMainnet.cosmosAPIEndpoint,
+              convertFee,
+              chain,
+              memo
+            );
+        setConvertState("Mining");
+        const confirmed = await checkCosmosTxConfirmation(
+          tx.tx_response.txhash
+        );
+        setConvertState(confirmed ? "Success" : "Fail");
+      } catch {
+        setConvertState("Exception");
+      }
+    };
+    const resetState = () => setConvertState("None");
+
+    return { state: convertState, send, resetState };
+  }
+  function useIBCTransfer(tokenDenom: string) {
+    const [ibcState, setIbcState] = useState<TransactionState>("None");
+    const send = async (
+      cosmosAddress: string,
+      amount: string,
+      bridgeOutNetwork: BridgeOutNetworkInfo
+    ) => {
+      //check to make sure the address not null
+      if (!bridgeOutNetwork.checkAddress(cosmosAddress)) {
+        setIbcState("Exception");
+        return;
+      }
+      setIbcState("PendingSignature");
+      try {
+        const tx = await txIBCTransfer(
+          cosmosAddress,
+          bridgeOutNetwork.channel,
+          amount,
+          tokenDenom,
+          CantoMainnet.cosmosAPIEndpoint,
+          bridgeOutNetwork.endpoint,
+          bridgeOutNetwork.latestBlockEndpoint,
+          ibcFee,
+          chain,
+          memo
+        );
+        setIbcState("Mining");
+        const confirmed = await checkCosmosTxConfirmation(
+          tx.tx_response.txhash
+        );
+        setIbcState(confirmed ? "Success" : "Fail");
+      } catch {
+        setIbcState("Exception");
+      }
+    };
+    const resetState = () => setIbcState("None");
+    return { state: ibcState, send, resetState };
+  }
+
+  return {
+    bridgeIn: {
+      approveToken: useApprove,
+      sendToCosmos: useSendToCosmos,
+    },
+    convertCoin: {
+      convertTx: useConvertCoin,
+    },
+    bridgeOut: {
+      ibcOut: useIBCTransfer,
+    },
+  };
+}
