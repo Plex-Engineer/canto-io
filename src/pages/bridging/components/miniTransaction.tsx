937599a4289826e4f03ecc7bb5feeb6c3a8205b3
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-03-14 01:34:12 +0530

    added confirm modal

--- src/pages/bridging/components/miniTransaction.tsx
+++ src/pages/bridging/components/miniTransaction.tsx
@@ -1,19 +1,62 @@
 import styled from "@emotion/styled";
 import { formatUnits } from "ethers/lib/utils";
+import GlobalLoadingModal from "global/components/modals/loadingModal";
+import { CantoTransactionType } from "global/config/transactionTypes";
 import { PrimaryButton, Text } from "global/packages/src";
+import Modal from "global/packages/src/components/molecules/Modal";
 import { formatBalance } from "global/utils/utils";
+import { useState } from "react";
 import { ConvertTransaction } from "../config/interfaces";
 import { BridgeTransaction } from "../hooks/useBridgingTransactions";
 import { convertSecondsToString } from "../utils/utils";
+import ConfirmationModal from "./modals/confirmationModal";
 
 interface Props {
   transaction: ConvertTransaction;
   txFactory: () => BridgeTransaction;
 }
 const MiniTransaction = (props: Props) => {
+  const [isModalOpen, setModalOpen] = useState(false);
   const txStats = props.txFactory();
+
+  function getTxStatus(): string {
+    switch (txStats.state) {
+      case "None":
+        return "complete";
+      case "Mining":
+        return "ongoing";
+      case "PendingSignature":
+        return "signing";
+      case "Success":
+        return "done";
+      case "Exception":
+      case "Fail":
+        return "error";
+      default:
+        return "complete";
+    }
+  }
+
   return (
     <Styled>
+      <Modal
+        title="confirmation"
+        open={isModalOpen}
+        onClose={() => {
+          if (txStats.state != "Mining" && txStats.state != "PendingSignature")
+            txStats.resetState();
+          setModalOpen(false);
+        }}
+      >
+        <ConfirmationModal
+          activeToken={props.transaction}
+          state={txStats.state}
+          onConfirm={() => {
+            txStats.send(props.transaction.amount.toString());
+          }}
+        />
+      </Modal>
+
       <div className="dual-item">
         <Text size="text3" align="left">
           origin
@@ -21,14 +64,16 @@ const MiniTransaction = (props: Props) => {
         <Text type="title">{props.transaction.origin}</Text>
       </div>
 
-      <div className="dual-item">
-        <Text size="text3" align="left">
-          time left
-        </Text>
-        <Text type="title" size="text2">
-          {convertSecondsToString(props.transaction.timeLeft)}
-        </Text>
-      </div>
+      {props.transaction.timeLeft != "0" && (
+        <div className="dual-item">
+          <Text size="text3" align="left">
+            time left
+          </Text>
+          <Text type="title" size="text2">
+            {convertSecondsToString(props.transaction.timeLeft)}
+          </Text>
+        </div>
+      )}
       <div className="dual-item">
         <Text size="text3" align="left">
           amount
@@ -43,22 +88,22 @@ const MiniTransaction = (props: Props) => {
           {" " + props.transaction.token.symbol}
         </Text>
       </div>
-      <PrimaryButton
-        style={{
-          maxWidth: "7rem",
-        }}
-        height="normal"
-        disabled={props.transaction.timeLeft !== "0"}
-        weight="bold"
-        filled
-        onClick={() => {
-          txStats.send(props.transaction.amount.toString());
-        }}
-      >
-        {props.transaction.timeLeft !== "0" ? "ongoing" : "complete"}
-      </PrimaryButton>
-      <button onClick={() => txStats.resetState()}>reset</button>
-      {txStats.state}
+      {props.transaction.timeLeft == "0" && (
+        <PrimaryButton
+          style={{
+            maxWidth: "7rem",
+          }}
+          height="normal"
+          disabled={props.transaction.timeLeft !== "0"}
+          weight="bold"
+          filled
+          onClick={() => {
+            setModalOpen(true);
+          }}
+        >
+          {props.transaction.timeLeft !== "0" ? "ongoing" : getTxStatus()}
+        </PrimaryButton>
+      )}
     </Styled>
   );
 };
