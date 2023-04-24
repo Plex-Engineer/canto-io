4240fea6410a037e04e30e63cadb16b19a6f419f
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-04-24 11:25:55 -0400

    remove cosmjs ref

--- src/pages/bridging/components/step1TxBox.tsx
+++ src/pages/bridging/components/step1TxBox.tsx
@@ -28,7 +28,7 @@ import Modal from "global/packages/src/components/molecules/Modal";
 import ConfirmationModal from "./modals/confirmationModal";
 import { CantoMainnet, ETHMainnet } from "global/config/networks";
 import { TokenWallet } from "./tokenSelect";
-import IBCGuideModal from "./modals/ibcGuideModal";
+// import IBCGuideModal from "./modals/ibcGuideModal";
 import { TokenGroups } from "global/config/tokenInfo";
 
 interface Step1TxBoxProps {
@@ -65,11 +65,12 @@ const Step1TxBox = (props: Step1TxBoxProps) => {
         open={isIBCModalOpen}
         onClose={() => setisIBCModalOpen(false)}
       >
-        <IBCGuideModal
+        {/* <IBCGuideModal
           token={selectedIBCToken}
           cantoAddress={props.toAddress ?? ""}
           onClose={() => setisIBCModalOpen(false)}
-        />
+        /> */}
+        <div>ref</div>
       </Modal>
       <Modal
         title="confirmation"
