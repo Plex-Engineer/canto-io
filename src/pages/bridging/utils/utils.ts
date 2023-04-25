2b38126a52896e0047f71ee8d54d7a07a709b2f9
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-04-25 14:07:44 -0400

    changed details function file

--- src/pages/bridging/utils/utils.ts
+++ src/pages/bridging/utils/utils.ts
@@ -6,8 +6,6 @@ import { truncateNumber } from "global/utils/utils";
 import { NativeTransaction, UserNativeToken } from "../config/interfaces";
 import { TransactionHistoryEvent } from "./bridgeTxHistory";
 import { getNetworkFromTokenName } from "./findTokens";
-import { ReactNode } from "react";
-import { Text } from "global/packages/src";
 
 export function createConvertTransactions(
   pendingIn: TransactionHistoryEvent[],
@@ -94,54 +92,4 @@ export function getStep1ButtonText(
   } else {
     return [bText, false];
   }
-}
-
-export function getBridgeExtraDetails(
-  bridgeIn: boolean,
-  native: boolean,
-  from: string,
-  to: string
-): ReactNode {
-  return (
-    <Text size="text4" align="left" style={{ color: "#474747" }}>
-      {native ? (
-        <>
-          {`by completing bridge ${
-            bridgeIn ? "in" : "out"
-          }, you are transferring your assets from your canto native address (${from}) to your ${
-            bridgeIn
-              ? "canto EVM address (" + to + ")"
-              : "address on the " + to + " network"
-          }. Read more about this `}
-          <a
-            role="button"
-            tabIndex={0}
-            onClick={() =>
-              window.open(
-                bridgeIn
-                  ? "https://docs.canto.io/user-guides/converting-assets"
-                  : "https://docs.canto.io/user-guides/bridging-assets/from-canto",
-                "_blank"
-              )
-            }
-            style={{
-              color: "var(--primary-color)",
-              cursor: "pointer",
-              textDecoration: "underline",
-            }}
-          >
-            here.
-          </a>
-        </>
-      ) : (
-        `by bridging ${
-          bridgeIn ? "in" : "out"
-        }, you are transferring your assets from your ${
-          bridgeIn ? "ethereum " : "canto "
-        } EVM address (${from}) to your canto native address (${to}) ${
-          bridgeIn ? "through gravity bridge." : "."
-        }`
-      )}
-    </Text>
-  );
 }
