7403c87275e0e252367d1d54bdb6eddccf579749
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-06-06 13:38:57 +0530

    remove console log

--- src/global/components/cantoNav.tsx
+++ src/global/components/cantoNav.tsx
@@ -18,15 +18,12 @@ export const CantoNav = () => {
   const networkInfo = useNetworkInfo();
   const alert = useAlert();
   const { activateBrowserWallet, account, chainId, active } = useEthers();
-  console.log("chainId", chainId);
   const balance = useEtherBalance(account);
   const cantoBalance = useEtherBalance(account, {
     chainId: getCantoNetwork(Number(networkInfo.chainId)).chainId,
   });
   const ethBalance = useEtherBalance(networkInfo.account, { chainId: 1 });
   const signer = useSigner();
-  console.log(signer)
-
   const canPubKey =
     (ethBalance?.gte(parseUnits("0.01")) ||
       networkInfo.balance?.gte(parseUnits("0.5"))) ??
